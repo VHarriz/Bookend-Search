7b6e3c95a8d3957416736696293d67d23672a1d0
VHarriz <128908597+VHarriz@users.noreply.github.com>
2023-06-06 14:27:10 -0400

    1 more

--- server/server.js
+++ server/server.js
@@ -10,9 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
+// add below two lines for deployment
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(routes);
 
